303d7787cb1460d854b6e4621dfe90a760b11419
grcodeman <grcodeman@gmail.com>
2025-06-19 07:17:40 -0700

    creating education motion section

--- app/page.tsx
+++ app/page.tsx
@@ -103,6 +103,17 @@ export default function Home() {
             </div>
           </motion.section>
 
+          <motion.section
+          initial={{opacity: 0, y: 100}}
+          animate={{opacity: 1, y:0}}
+          transition={{duration: 0.8}}
+          
+          className=""
+          >
+            <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">Education</h2>
+
+          </motion.section>
+
         </main>
 
       </div>
