e4faad0d65cb8184a8b0dd505319d8ec332a74cb
grcodeman <grcodeman@gmail.com>
2025-09-19 16:49:47 -0400

    adding clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tdd94hwaep");
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${inter.className}  antialiased`}
       >
